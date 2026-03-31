fa26cbfad8e762f431fe29f6613bb9e8349380d3
Mateo-2429 <57055664+Mateo-0615@users.noreply.github.com>
2026-03-30 22:38:57 -0500

    Update gasto.dart

--- lib/models/gasto.dart
+++ lib/models/gasto.dart
@@ -1,1 +1,13 @@
+class Gasto {
+  final String categoria;
+  final double monto;
+  final String descripcion;
+  final DateTime fecha;
 
+  Gasto({
+    required this.categoria,
+    required this.monto,
+    required this.descripcion,
+    required this.fecha,
+  });
+}
