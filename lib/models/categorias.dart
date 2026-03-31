d2793fc5003bac3903424fba08b6a264a40e6c5b
Mateo-2429 <57055664+Mateo-0615@users.noreply.github.com>
2026-03-30 22:38:33 -0500

    Update categorias.dart

--- lib/models/categorias.dart
+++ lib/models/categorias.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class Categoria {
+  final String nombre;
+  final IconData icono;
+
+  Categoria({
+    required this.nombre,
+    required this.icono,
+  });
+}
